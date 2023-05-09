992873ea692631f4874e9289ce1b2f6a50219021
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-09 16:20:46 +0300

    Add UserSerializer

--- djangoapi/snippets/admin.py
+++ djangoapi/snippets/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Snippet
 
 
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ("highlighted",)
+
 admin.site.register(Snippet)
--- djangoapi/snippets/serializers.py
+++ djangoapi/snippets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
@@ -13,3 +14,15 @@ class SnippetSerializer(serializers.ModelSerializer):
             "language",
             "style",
         )
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "snippets")
+        
+        
