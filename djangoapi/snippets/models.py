20fefc491a102844b84c994e7323eff7d15bb2b6
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-09 16:12:22 +0300

    Add two new field to Snippets and make migrations

--- djangoapi/snippets/models.py
+++ djangoapi/snippets/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
@@ -16,9 +19,27 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ["created"]
+        
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
