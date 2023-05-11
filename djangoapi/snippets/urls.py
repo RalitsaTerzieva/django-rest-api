36859054b452f0161a46b9307d035811537b6688
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-11 21:02:01 +0300

    Add Root API endpoint

--- djangoapi/snippets/urls.py
+++ djangoapi/snippets/urls.py
@@ -3,10 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path("snippets/", views.SnippetList.as_view()),
-    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
+    path("",views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- djangoapi/snippets/views.py
+++ djangoapi/snippets/views.py
@@ -1,9 +1,24 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer,UserSerializer
+
 from django.contrib.auth.models import User
 
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
