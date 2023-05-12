ee1411a8ea38d7e231dec268bcf425a7ea85bb49
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-12 14:34:23 +0300

    Highlighed snippets endpoint

--- djangoapi/snippets/views.py
+++ djangoapi/snippets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -9,6 +9,14 @@ from .serializers import SnippetSerializer,UserSerializer
 
 from django.contrib.auth.models import User
 
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 @api_view(["GET"])
